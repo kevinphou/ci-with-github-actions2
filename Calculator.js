a253bc124ffdc9b6ad65cddd8a737d0e78554daa
kevinphou <>
2023-03-14 17:31:12 +0000

    Adds testing

---
+++ Calculator.js
@@ -0,0 +1,18 @@
+class Calculator{
+
+  constructor()
+  {
+	console.log("Calculator loaded");
+  }
+
+  add(num1,num2){
+	   return num1 + num2;
+  }
+
+}
+
+//required so data is visible externally
+module.exports = Calculator;
+
+
+
